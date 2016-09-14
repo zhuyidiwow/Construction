78ec2084bfe808b9e6b6523b1834acc9ce11629a
AndyZhu <zhuyidiwow@gmail.com>
2016-09-14 22:17:13 +0800

    formwork model without physics

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	private float moveSpeed = 3f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Vertical") != 0) {
+			//this.GetComponent <Rigidbody>
+			Transform thisT = this.transform;
+			transform.position = new Vector3 (thisT.position.x + moveSpeed * Time.deltaTime, thisT.position.y, thisT.position.z);
+		}
+	}
+}
